216a28e843091fc183d63ed6fd1189c0affe4650
rocker bell <rockbell8@gmail.com>
2024-12-10 17:11:33 +0100

    Add main page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./style.css"
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+export default function App() {
+  return  (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <form className="new-item-form form">
+      <div className="form-item">
+        <label htmlFor="item">Enter</label>
+        <input type="text" id="item" />
+    </div>
+    <button className="btn"> submit</button>
+    </form>
+    <h3>submitted items:</h3>
+    <div className="itemcheck">
+      <div className="item1">
+        <input type="checkbox" name="" id="item1" />
+        <label htmlFor="item1">item1</label>
+        <button className="btn btn-danger">Remove</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      <div className="item1">
+        <input type="checkbox" name="" id="item1" />
+        <label htmlFor="item1">item2</label>
+        <button className="btn btn-danger">Remove</button>
+      </div>
+    </div>
+    </>  
+  )
+  
+}
